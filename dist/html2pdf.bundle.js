d2db342e9eda7e29ace1d6074c01008ac846930c
Houke de Kwant <houke@ivaldi.nl>
2019-04-18 13:13:42 +0200

    add page option to toContainer

--- dist/html2pdf.bundle.js
+++ dist/html2pdf.bundle.js
@@ -1,6 +1,6 @@
 /**
  * html2pdf.js v0.9.1
- * Copyright (c) 2018 Erik Koopmans
+ * Copyright (c) 2019 Erik Koopmans
  * Released under the MIT License.
  */
 (function (global, factory) {
@@ -9281,55 +9281,57 @@ Worker.prototype.toContainer = function toContainer() {
 // Main link array, and refs to original functions.
 var linkInfo = [];
 var orig$1 = {
-  toContainer: Worker.prototype.toContainer,
-  toPdf: Worker.prototype.toPdf
+	toContainer: Worker.prototype.toContainer,
+	toPdf: Worker.prototype.toPdf
 };
 
-Worker.prototype.toContainer = function toContainer() {
-  return orig$1.toContainer.call(this).then(function toContainer_hyperlink() {
-    // Retrieve hyperlink info if the option is enabled.
-    if (this.opt.enableLinks) {
-      // Find all anchor tags and get the container's bounds for reference.
-      var container = this.prop.container;
-      var links = container.querySelectorAll('a');
-      var containerRect = unitConvert(container.getBoundingClientRect(), this.prop.pageSize.k);
-      linkInfo = [];
+Worker.prototype.toContainer = function toContainer(page) {
+	return orig$1.toContainer.call(this).then(function toContainer_hyperlink() {
+		// Retrieve hyperlink info if the option is enabled.
+		if (this.opt.enableLinks) {
+			// Find all anchor tags and get the container's bounds for reference.
+			var container = this.prop.container;
+			var links = container.querySelectorAll('a');
+			var containerRect = unitConvert(container.getBoundingClientRect(), this.prop.pageSize.k);
+			linkInfo = [];
 
-      // Loop through each anchor tag.
-      Array.prototype.forEach.call(links, function (link) {
-        // Treat each client rect as a separate link (for text-wrapping).
-        var clientRects = link.getClientRects();
-        for (var i = 0; i < clientRects.length; i++) {
-          var clientRect = unitConvert(clientRects[i], this.prop.pageSize.k);
-          clientRect.left -= containerRect.left;
-          clientRect.top -= containerRect.top;
+			// Loop through each anchor tag.
+			Array.prototype.forEach.call(links, function (link) {
+				// Treat each client rect as a separate link (for text-wrapping).
+				var clientRects = link.getClientRects();
+				for (var i = 0; i < clientRects.length; i++) {
+					var clientRect = unitConvert(clientRects[i], this.prop.pageSize.k);
+					clientRect.left -= containerRect.left;
+					clientRect.top -= containerRect.top;
 
-          var page = Math.floor(clientRect.top / this.prop.pageSize.inner.height) + 1;
-          var top = this.opt.margin[0] + clientRect.top % this.prop.pageSize.inner.height;
-          var left = this.opt.margin[1] + clientRect.left;
+					if (typeof page === 'undefined') {
+						page = Math.floor(clientRect.top / this.prop.pageSize.inner.height) + 1;
+					}
+					var top = this.opt.margin[0] + clientRect.top % this.prop.pageSize.inner.height;
+					var left = this.opt.margin[1] + clientRect.left;
 
-          linkInfo.push({ page: page, top: top, left: left, clientRect: clientRect, link: link });
-        }
-      }, this);
-    }
-  });
+					linkInfo.push({ page: page, top: top, left: left, clientRect: clientRect, link: link });
+				}
+			}, this);
+		}
+	});
 };
 
 Worker.prototype.toPdf = function toPdf() {
-  return orig$1.toPdf.call(this).then(function toPdf_hyperlink() {
-    // Add hyperlinks if the option is enabled.
-    if (this.opt.enableLinks) {
-      // Attach each anchor tag based on info from toContainer().
-      linkInfo.forEach(function (l) {
-        this.prop.pdf.setPage(l.page);
-        this.prop.pdf.link(l.left, l.top, l.clientRect.width, l.clientRect.height, { url: l.link.href });
-      }, this);
+	return orig$1.toPdf.call(this).then(function toPdf_hyperlink() {
+		// Add hyperlinks if the option is enabled.
+		if (this.opt.enableLinks) {
+			// Attach each anchor tag based on info from toContainer().
+			linkInfo.forEach(function (l) {
+				this.prop.pdf.setPage(l.page);
+				this.prop.pdf.link(l.left, l.top, l.clientRect.width, l.clientRect.height, { url: l.link.href });
+			}, this);
 
-      // Reset the active page of the PDF to the final page.
-      var nPages = this.prop.pdf.internal.getNumberOfPages();
-      this.prop.pdf.setPage(nPages);
-    }
-  });
+			// Reset the active page of the PDF to the final page.
+			var nPages = this.prop.pdf.internal.getNumberOfPages();
+			this.prop.pdf.setPage(nPages);
+		}
+	});
 };
 
 /**
